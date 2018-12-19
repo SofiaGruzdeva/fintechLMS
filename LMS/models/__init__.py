f26d32b24befe92b77384943e4e87f782fc76954
SofiaGruzdeva <gruzdeva@phystech.edu>
2018-12-19 20:25:09 +0300

    Delete LMS
    Delete old version of LMS

--- LMS/models/__init__.py
+++
@@ -1,9 +0,0 @@
-from flask_sqlalchemy import SQLAlchemy
-
-# initialize our db
-db = SQLAlchemy()
-
-
-from .UserModel import UserModel, UserSchema
-from .StudyGroup import StudyGroup, StudyGroupShema
-from .StudyCourse import StudyCourse, StudyCourseShema
--- LMS/tests/test_user.py
+++
@@ -1,8 +0,0 @@
-import pytest
-import os
-import json
-from ..app import create_app, db
-
-def test_hello(client):
-    response = client.get('/')
-    assert response.data == b'Congratulations! Your part 2 endpoint is working'
